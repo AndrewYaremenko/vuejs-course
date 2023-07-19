d61a0927a564a34cf843c002e21ef5ac7fd017d2
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-19 17:51:04 +0300

    component, interpolation, methods: v-on listener

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <button v-on:click="addLike">Like</button>
+            <button v-on:click="addDislike">Dislike</button>
+        </div>
+        <div>Likes: <strong>{{ likes }}</strong></div>
+        <div>Dislikes: <strong>{{ dislikes }}</strong></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 5,
+            dislikes: 2,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes++;
+        },
+        addDislike() {
+            this.dislikes++
+        }
+    }
+}
+</script>
+
+<style></style>
