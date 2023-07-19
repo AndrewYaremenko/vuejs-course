86f6b521266cab3e93597567d493b1c7ecf7a8fe
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-19 23:29:26 +0300

    bilateral binding, prevent modifier

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
+    <div class="container">
+        <form @submit.prevent>
+            <h4>Create new post</h4>
+            <input v-bind:value="title" @input="title = $event.target.value" type="text" class="input" name="title"
+                placeholder="Title">
+            <input v-bind:value="description" @input="inputDescription" type="text" class="input" name="description"
+                placeholder="Description">
+            <button class="btn" @click="createPost">Create</button>
+        </form>
         <div class="post" v-for="post in posts">
-            <div><strong>Title: </strong> {{post.title}}</div>
-            <div><strong>Description: </strong> {{post.description}}</div>
+            <div><strong>Title: </strong> {{ post.title }}</div>
+            <div><strong>Description: </strong> {{ post.description }}</div>
+
         </div>
     </div>
 </template>
@@ -12,14 +21,28 @@ export default {
     data() {
         return {
             posts: [
-                {id: 1, title: "Lorem, ipsum.", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit."},
-                {id: 2, title: "Lorem ipsum dolor sit amet.", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, quia?"},
-                {id: 3, title: "Lorem, ipsum dolor.", description: "Lorem ipsum dolor sit amet consectetur adipisicing."},
-            ]
+                { id: 1, title: "Lorem, ipsum.", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit." },
+                { id: 2, title: "Lorem ipsum dolor sit amet.", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, quia?" },
+                { id: 3, title: "Lorem, ipsum dolor.", description: "Lorem ipsum dolor sit amet consectetur adipisicing." },
+            ],
+            title: '',
+            description: '',
         }
     },
     methods: {
-
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                description: this.description,
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.description= '';
+        },
+        inputDescription(event) {
+            this.description = event.target.value;
+        }
     }
 }
 </script>
@@ -31,9 +54,34 @@ export default {
     box-sizing: border-box;
 }
 
+.container {
+    margin: 30px;
+}
+
 .post {
     padding: 15px;
     margin-top: 15px;
     border: 3px solid darkgreen;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.input {
+    width: 100%;
+    border: 1px solid darkgreen;
+    padding: 15px;
+    margin-top: 15px;
+}
+
+.btn {
+    margin-top: 15px;
+    padding: 10px 15px;
+    align-self: flex-end;
+    color: teal;
+    background: none;
+    border: 1px solid darkgreen;
 }
 </style>
