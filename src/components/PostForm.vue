223b8cb4f444fde4dadc046467c95c1367e3db4a
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-20 13:05:43 +0300

    decomposition, passing data to a component

---
+++ src/components/PostForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <form @submit.prevent>
+    <h4>Create new post</h4>
+    <input
+      v-bind:value="post.title"
+      @input="post.title = $event.target.value"
+      type="text"
+      class="input"
+      name="title"
+      placeholder="Title"
+    />
+    <input
+      v-bind:value="post.description"
+      @input="post.description = $event.target.value"
+      type="text"
+      class="input"
+      name="description"
+      placeholder="Description"
+    />
+    <button class="btn" @click="createPost">Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            post: {
+                title: '',
+                description:  '',
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid darkgreen;
+  padding: 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  align-self: flex-end;
+  color: teal;
+  background: none;
+  border: 1px solid darkgreen;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Title: </strong> {{ post.title }}</div>
+    <div><strong>Description: </strong> {{ post.description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  margin-top: 15px;
+  border: 3px solid darkgreen;
+}
+</style>
